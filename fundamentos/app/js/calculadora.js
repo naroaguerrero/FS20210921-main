f990f6eb672987c05ae2c8ea4492203e365f7794
naroaguerrero <naroaguerrero@gmail.com>
2021-09-30 13:41:37 +0200

    calculadora

--- fundamentos/app/js/calculadora.js
+++ fundamentos/app/js/calculadora.js
@@ -1,15 +1,17 @@
 /* eslint-disable no-unused-vars */
-var Calculadora = {
-    suma: function (num1, num2) {
+
+   function suma(num1, num2) {
         return num1 + num2;
-    },
-    resta: function (num1, num2) {
+    }
+
+   function resta(num1, num2) {
         return num1 - num2;
-    },
-    multiplicacion: function (num1, num2) {
+    }
+
+    function multilicacion(num1, num2) {
         return num1 * num2;
-    },
-    division: function (num1, num2) {
+    }
+
+     function division(num1, num2) {
         return num2 && num1/num2;
     }
-};
