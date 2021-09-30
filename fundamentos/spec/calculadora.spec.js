714ba1fa65111b4ecd60736dc6f1d71137bb45f1
naroaguerrero <naroaguerrero@gmail.com>
2021-09-30 15:46:04 +0200

    calculadora

--- fundamentos/spec/calculadora.spec.js
+++ fundamentos/spec/calculadora.spec.js
@@ -12,11 +12,11 @@ describe('Calculadora',()=> {
         expect(Calculadora.resta(number1, number2)).toBe(number1 - number2);
     });
 
-    it(number1 + 'x' + number2 + ' = ' + (number1*number2), function () {
+    it(number1 + ' x ' + number2 + ' = ' + (number1*number2), function () {
        expect(Calculadora.multiplicacion(number1, number2)).toBe(number1 * number2);
     });
 
-    it(number1 + '/' + number2 + ' = ' + (number2 && number1/number2), function () {
+    it(number1 + ' / ' + number2 + ' = ' + (number2 && number1/number2), function () {
         expect(Calculadora.division(number1, number2)).toBe(number2 && number1/number2);
     });
 });
