f09af70728e1c62f9be2438079703214cce8a102
naroaguerrero <naroaguerrero@gmail.com>
2021-10-07 15:05:57 +0200

    formulario

--- angular/src/app/formulario/formulario.component.ts
+++ angular/src/app/formulario/formulario.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Injectable } from '@angular/core';
+import { NotificationService, NotificationType } from '../common-services';
 
 export interface Persona {
   id: number | null;
@@ -7,21 +8,63 @@ export interface Persona {
   apellidos: string;
   correo: string | null;
   edad: number | null;
-  dni:  string | null
+  dni: string | null;
 }
 
-@Injectable({ providedIn: 'root' })
-export class ClientesViewModel {
-  constructor() {}
+@Injectable({providedIn: 'root'})
+export class PersonasViewModel {
+  Listado: Array<Persona> = [
+    { id: 1, nombre: 'Pepito', apellidos: 'Grillo', correo: 'pepito@grillo', edad: 99, dni: '12345678Z' }
+  ]
+  Elemento: Persona = { id: null, nombre: '', apellidos: '', correo: null, edad: null, dni: null };
+  IsAdd = true;
+
+  constructor(private notify: NotificationService) {
+
+  }
+
+  public list() {
+
+  }
+
+  public add() {
+    this.Elemento = { id: null, nombre: '', apellidos: '', correo: null, edad: null, dni: null }
+    this.IsAdd = true;
+  }
+
+  public edit() {
+    this.Elemento = this.Listado[0];
+    this.IsAdd = false;
+  }
+
+  public view() {
+    this.Elemento = this.Listado[0];
+    this.IsAdd = false;
+  }
+
+  public delete() {
+
+  }
+
+  public cancel() {
+
+  }
+
+  public send() {
+    this.notify.add((this.IsAdd ? 'Nuevos: ' : 'Modificados: ') + JSON.stringify(this.Elemento), NotificationType.info);
+  }
 }
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
-  styleUrls: ['./formulario.component.scss'],
+  styleUrls: ['./formulario.component.scss']
 })
 export class FormularioComponent implements OnInit {
-  constructor(public vm: ClientesViewModel) {}
 
-  ngOnInit(): void {}
+  constructor(public vm: PersonasViewModel) { }
+
+  ngOnInit(): void {
+  }
+
 }
